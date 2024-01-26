2e322bc0db70f81c98a7bbccb761730e6e25904f
koendamme <koen.damme1999@gmail.com>
2024-01-26 13:19:09 +0100

    Created kp point detection

--- adversarial_dataset.py
+++ adversarial_dataset.py
@@ -6,30 +6,33 @@ from torch.utils.data import Dataset
 
 
 class AdversarialDataset(Dataset):
-    def __init__(self, annotation_file, categories_file, img_dir, img_extension,x_transform=None, y_transform=None):
+    def __init__(self, annotation_file, categories_file, img_dir, noise_dir, img_transform=None, noise_transform=None):
         self.img_dir = img_dir
-        self.img_extension = img_extension
+        self.noise_dir = noise_dir
         annotations = pd.read_csv(annotation_file)
         self.categories = pd.read_csv(categories_file)
         self.images = annotations["ImageId"]
         self.labels = annotations["TrueLabel"]
         self.targets = annotations["TargetClass"]
-        self.x_transform = x_transform
-        self.y_transform = y_transform
+        self.img_transform = img_transform
+        self.noise_transform = noise_transform
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.images[idx] + self.img_extension)
-        if self.img_extension == ".png":
-            image = plt.imread(img_path)
-        else:
-            with open(img_path, 'rb') as f:
-                image = np.load(f)
-                image = np.transpose(image, (1, 2, 0))
+        img_path = os.path.join(self.img_dir, self.images[idx] + ".png")
 
-        if self.x_transform:
-            image = self.x_transform(image)
+        image = plt.imread(img_path)
 
-        return image, self.labels[idx] - 1, self.targets[idx] - 1, self.images[idx]
+        noise = np.random.normal(0, 1, image.shape)
+        # with open(img_path, 'rb') as f:
+        #     noise = np.load(f)
+
+        if self.img_transform:
+            image = self.img_transform(image)
+
+        if self.noise_transform:
+            noise = self.noise_transform(noise)
+
+        return image, noise, self.labels[idx] - 1, self.targets[idx] - 1, self.images[idx]
